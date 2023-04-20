80b024218609d1134a96e975ca0df85227fb64b9
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-04-20 11:56:12 +0600

    Create a variable using a user-defined name

--- JS.js
+++ JS.js
@@ -182,4 +182,10 @@ fToC(45);
 console.log(document.URL);
 
 
- 
+// Problem #13:
+// Write a JavaScript exercise to create a variable using a user-defined name.
+
+var var_name = 'abcd';
+var n = 120;
+this[var_name] = n;
+console.log(this[var_name])
