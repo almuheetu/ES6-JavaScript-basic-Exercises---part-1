868ecbaa92ca5b3935d36148fd772e7cf8520a9a
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-04-20 12:11:55 +0600

    get the difference between a given number and 13, if the number is broader than 13 return double the absolute difference.

--- JS.js
+++ JS.js
@@ -202,7 +202,17 @@ console.log(filename.split('.').pop());
 
 
 // Problem #15:
-// Write a JavaScript exercise to create a variable using a user-defined name.
+// Write a JavaScript program to get the difference between a given number and 13, if the number is broader than 13 return double the absolute difference.
+
+function difference(n)
+ {
+    if (n <= 13)
+        return 13 - n;
+    else
+        return (n - 13) * 2;
+ }
+console.log(difference(32))
+console.log(difference(11))
 
 
 
