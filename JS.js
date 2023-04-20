b97473bf0d3db8be0ea27613814cd98b632f5337
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-04-20 17:10:13 +0600

    JavaScript program to compute the absolute difference between a specified number and 19.  Returns triple their absolute difference if the specified number is greater than 19.

--- JS.js
+++ JS.js
@@ -235,7 +235,21 @@ function sumTriple (x, y) {
 
 
 // Problem #17:
-// Write a JavaScript exercise to create a variable using a user-defined name.
+// Write a JavaScript program to compute the absolute difference between a specified number and 19. Returns triple the absolute difference if the specified number is greater than 19.
+
+function diff_num(n) {
+    if (n <= 19) {
+      return (19 - n);
+      }
+    else
+      {
+       return (n - 19) * 3;
+      }
+  }
+  
+  console.log(diff_num(12));
+  console.log(diff_num(19));
+  console.log(diff_num(22));
 
 
 
