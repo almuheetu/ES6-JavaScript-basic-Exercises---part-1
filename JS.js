aaa8996b204fbde90fba0462152c303b9b788e06
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-04-20 11:45:15 +0600

    convert temperatures to and from celsius, fahrenhei

--- JS.js
+++ JS.js
@@ -154,4 +154,26 @@ function divideBy() {
     document.getElementById("result").innerHTML = num1 / num2;
 }
 
+// Problem #11:
+// Write a JavaScript program to convert temperatures to and from Celsius, Fahrenheit.
 
+function cToF(celsius) 
+{
+  var cTemp = celsius;
+  var cToFahr = cTemp * 9 / 5 + 32;
+  var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+    console.log(message);
+}
+
+function fToC(fahrenheit) 
+{
+  var fTemp = fahrenheit;
+  var fToCel = (fTemp - 32) * 5 / 9;
+  var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+    console.log(message);
+} 
+cToF(60);
+fToC(45);
+
+
+ 
