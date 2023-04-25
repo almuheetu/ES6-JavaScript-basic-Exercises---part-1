8aaebe905b6a93d3e21858ce89fcd4efc1b48a8a
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-04-25 15:47:17 +0600

    JavaScript program to check whether a given integer is within 20 of 100 or 400.

--- JS.js
+++ JS.js
@@ -264,7 +264,18 @@ console.log(test50(20, 30))
 
 
 
-// Problem #13:
-// Write a JavaScript exercise to create a variable using a user-defined name.
+// Problem #19:
+// Write a JavaScript program to check whether a given integer is within 20 of 100 or 400.
+
+function testhundred(x) {
+    return ((Math.abs(100 - x) <= 20) ||
+       (Math.abs(400 - x) <= 20));
+  }
+  
+  console.log(testhundred(10));
+  console.log(testhundred(90));
+  console.log(testhundred(99));
+  console.log(testhundred(199));
+  console.log(testhundred(200));
 
 
