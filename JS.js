f394db2a7941ce579737c18eac5468a621b79aab
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-04-30 14:48:46 +0600

    JavaScript program to create a new string from a given string changing the position of first and last characters.

--- JS.js
+++ JS.js
@@ -338,7 +338,21 @@ console.log(remove_character("Python",5));
 
 
 // Problem #23:
-// Write a JavaScript program to check whether a given integer is within 20 of 100 or 400.
+//  Write a JavaScript program to create a new string from a given string by changing the position of the first and last characters. The string length must be broader than or equal to 1
+
+function first_last(str1) 
+  {
+  if (str1.length <= 1)
+  {
+    return str1;
+  }
+  mid_char = str1.substring(1, str1.length - 1);
+  return (str1.charAt(str1.length - 1)) + mid_char + str1.charAt(0);
+}
+console.log(first_last('a'));
+console.log(first_last('ab'));
+console.log(first_last('abc'));
+
 
 
 // Problem #24:
