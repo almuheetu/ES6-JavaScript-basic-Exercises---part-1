e47e9fe652380970f85774f4a5be77edb5ea3b59
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-05-01 17:02:00 +0600

    JavaScript program to find the largest of three given integers.

--- JS.js
+++ JS.js
@@ -478,7 +478,29 @@ console.log(check_script("CoffeeScript"));
 
 
 // Problem #31:
-// Write a JavaScript program to check whether a string starts with 'Java' if it does not otherwise.
+// Write a JavaScript program to find the largest of three given integers.
+
+function max_of_three(x, y, z) 
+ {
+  max_val = 0;
+  if (x > y)
+  {
+    max_val = x;
+  } else
+  {
+    max_val = y;
+  }
+  if (z > max_val) 
+  {
+    max_val = z;
+  }
+  return max_val;
+}
+
+console.log(max_of_three(1,0,1));
+console.log(max_of_three(0,-10,-20));
+console.log(max_of_three(1000,510,440));
+
 
 
 // Problem #32:
