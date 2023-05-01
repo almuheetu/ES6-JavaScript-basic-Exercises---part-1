287aa00163a0512eee9a3696037c78715e29a50d
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-05-01 18:22:17 +0600

    JavaScript program to find the larger number from the two given positive integers, the two numbers  are in the range 40..60 inclusive

--- JS.js
+++ JS.js
@@ -544,7 +544,26 @@ console.log(numbers_ranges(50, 89));
 
 
 // Problem #34:
-// Write a JavaScript program to find the closest value to 100 from two numerical values
+// Write a JavaScript program to find the largest number from the two given positive integers. The two numbers are in the range 40..60 inclusive. 
+
+function max_townums_range(x, y) {
+    if ((x >= 40) && (x <= 60) && (y >= 40 && y <= 60)) {
+        if (x === y) {
+            return "Numbers are the same";
+        } else if (x > y) {
+            return x;
+        } else {
+            return y;
+        }
+    } else {
+        return "Numbers don't fit in range";
+    }
+}
+
+console.log(max_townums_range(45, 60));
+console.log(max_townums_range(25, 60));
+console.log(max_townums_range(45, 80));
+
 
 
 
