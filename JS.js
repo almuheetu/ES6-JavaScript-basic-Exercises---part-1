9f7f0fba8d14411de34bbd7225ad5c5a660483fd
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-05-02 16:36:41 +0600

    JavaScript program to check a given string contains 2 to 4 numbers of a specified character.

--- JS.js
+++ JS.js
@@ -568,7 +568,26 @@ console.log(max_townums_range(45, 80));
 
 
 // Problem #35:
-// Write a JavaScript program to find the closest value to 100 from two numerical values
+// Write a program to check whether a specified character exists between the 2nd and 4th positions in a given string.
+
+function check_char(str1, char) {
+    ctr = 0;
+    for (let i = 0; i < str1.length; i++) {
+        if ((str1.charAt(i) == char) && (i >= 1 && i <= 3)) {
+            ctr = 1;
+            break;
+        }
+    }
+    if (ctr == 1) return true;
+    return false;
+}
+console.log(check_char("Python", "y"));
+console.log(check_char("JavaScript", "a"));
+console.log(check_char("Console", "o"));
+console.log(check_char("Console", "C"));
+console.log(check_char("Console", "e"));
+console.log(check_char("JavaScript", "S"));
+
 
 
 // Problem #36:
