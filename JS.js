c813ee9c4e9b5379a3140307a85fbe03143b121e
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-05-04 20:43:31 +0600

    JavaScript program to check the total marks of a student in various examinations

--- JS.js
+++ JS.js
@@ -632,4 +632,16 @@ function upper_lower(str) {
 
 
 // Problem #38:
-// Write a JavaScript program to find the closest value to 100 from two numerical values
+// Write a JavaScript program to check a student's total marks in various examinations. The student will get A+ grade if the total marks are in the range 89..100 inclusive, if the examination is "Final-exam" the student will receive A+ grade and total marks must be greater than or equal to 90. If the student gets an A+ grade, return true, otherwise return false.
+
+function exam_status(totmarks,is_exam)
+  {
+  if (is_exam) {
+    return totmarks >= 90;
+  }
+ return (totmarks >= 89 && totmarks <= 100);
+ }
+
+console.log(exam_status("78", " "));
+console.log(exam_status("89", "true "));
+console.log(exam_status("99", "true "));
