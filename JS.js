116913b6b7631b3e71333376a1ed732f0e714015
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-05-06 15:55:55 +0600

    JavaScript program to check whether a given number is presents in the range 40..10000

--- JS.js
+++ JS.js
@@ -802,7 +802,23 @@ console.log(valCheck(16, 20));
 
 
 // Problem #47:
-// Write a JavaScript program that checks whether the last digit of three positive integers is the same.
+// Write a JavaScript program to check whether a given number exists in the range 40..10000
+
+function test_digit(x, y, n) 
+  {    
+    if (n < 40 || n > 10000)
+      return false;
+    else
+      if (n >= x && n <= y)
+        return true;
+      else
+        return false;
+  }
+console.log(test_digit(40, 4000, 45));  
+console.log(test_digit(80, 320, 79));  
+console.log(test_digit(89, 4000, 30));
+
+
 
 
 
