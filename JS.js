4a6876220537e655c6068907ac58501a9d32b402
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-05-06 16:10:17 +0600

    JavaScript program to capitalize the first letter of each word of a given string

--- JS.js
+++ JS.js
@@ -841,7 +841,21 @@ console.log(LetterChanges("php"));
 
 
 // Problem #50:
-// Write a JavaScript program that checks whether the last digit of three positive integers is the same.
+// Write a JavaScript program to capitalize the first letter of each word in a given string.
+
+
+function capital_letter(str) {
+    str = str.split(" ");
+
+    for (var i = 0, x = str.length; i < x; i++) {
+        str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+    }
+
+    return str.join(" ");
+}
+
+console.log(capital_letter("Write a JavaScript program to capitalize the first letter of each word of a given string."));
+
 
 
 
