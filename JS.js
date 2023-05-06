5c11e1d6ff82953e04a433eb0d55ac937ab7b191
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-05-06 15:59:13 +0600

    Write a JavaScript program that checks whether the last digit of three positive integers is the same.

--- JS.js
+++ JS.js
@@ -825,14 +825,27 @@ console.log(test_digit(89, 4000, 30));
 
 
 // Problem #48:
-// Write a JavaScript program that checks whether the last digit of three positive integers is the same.
+// Write a JavaScript program to reverse a given string.
+
+function string_reverse(str) 
+{
+    return str.split("").reverse().join("");
+}
+
+console.log(string_reverse("w3resource"));
+console.log(string_reverse("www"));
+console.log(string_reverse("JavaScript"));
+
+
 
 
 
 
 
 // Problem #49:
-// Write a JavaScript program that checks whether the last digit of three positive integers is the same.
+// Write a JavaScript program to replace every character in a given string with the character following it in the alphabet.
+
+
 
 
 
