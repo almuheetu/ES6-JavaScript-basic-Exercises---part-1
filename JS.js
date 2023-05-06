24c611853960e7d0efe98b5b16543c122714db20
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-05-06 14:00:16 +0600

    JavaScript program to check two given integer values and return true if one of the number is 15 or if their sum or difference is 15.

--- JS.js
+++ JS.js
@@ -764,7 +764,20 @@ console.log(lessby20_others(21, 66, 75));
 
 
 // Problem #45:
-// Write a JavaScript program that checks whether the last digit of three positive integers is the same.
+// Write a JavaScript program that checks two integer values and returns true if either one is 15 or if their sum or difference is 15.
+
+function test_number(x, y) {
+    return (x === 15 || y === 15 || x + y === 15 || Math.abs(x - y) === 15);
+}
+
+console.log(test_number(15, 9));
+console.log(test_number(25, 15));
+console.log(test_number(7, 8));
+console.log(test_number(25, 10));
+console.log(test_number(5, 9));
+console.log(test_number(7, 9));
+console.log(test_number(9, 25));
+
 
 
 
