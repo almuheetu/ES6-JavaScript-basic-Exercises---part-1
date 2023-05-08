8623367aba78fd40e1f3d8a0b86fbea075ec8658
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-05-08 10:37:27 +0600

    check whether a given string contains equal number of p's and t's

--- JS.js
+++ JS.js
@@ -924,7 +924,23 @@ console.log(vowel_Count("w3resource.com"));
 
 
 // Problem #55:
-// Write a JavaScript program to convert letters of a given string alphabetically.
+// Write a JavaScript program to check whether a given string contains an equal number of p's and t's
+
+function equal_pt(str)
+{ 
+  var str_p = str.replace(/[^p]/g, "");
+
+  var str_t = str.replace(/[^t]/g, "");
+
+  var p_num = str_p.length;
+  var s_num = str_t.length;
+
+  return p_num === s_num;
+         
+}
+console.log(equal_pt("paatpss"));
+console.log(equal_pt("paatps"));
+
 
 
 
