c30a11b4b20cba35f17a747d62f74b3148a518ce
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-05-09 14:08:51 +0600

    JavaScript program to move last three character to the start of a given string. The string length must be 3.

--- JS.js
+++ JS.js
@@ -1019,7 +1019,19 @@ function concatenate(str1, str2) {
 
 
 // Problem #62:
-// Write a JavaScript program to convert letters of a given string alphabetically.
+// Write a JavaScript program to move the last three characters to the start of a given string. The string length must be greater than or equal to three. 
+
+function right_three(str) {
+    if (str.length > 1)
+      {
+        return str.slice(-3) + str.slice(0, -3);
+      }
+ return str;
+}
+console.log(right_three("Python"));
+console.log(right_three("JavaScript"));
+console.log(right_three("Hi"));
+
 
 
 // Problem #63:
