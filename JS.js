ea98484bf9af44ffeaf79e4234946c7e5ac38042
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-05-10 14:14:20 +0600

    JavaScript program to create a new string from a given string, removing the first and last characters of the string if the first or  last character are �P<

--- JS.js
+++ JS.js
@@ -1103,7 +1103,29 @@ console.log(city_name("London"));
 
 
 // Problem #67:
-// Write a JavaScript program to convert letters of a given string alphabetically.
+// Write a JavaScript program to create a new string from a given string. This program removes the first and last characters of the string if the first or last character is 'P'. Return the original string if the condition is not satisfied.
+
+function nop(str) {
+    let start_pos = 0;
+    let end_pos = str.length;
+  
+    if (str.length > 0 && str.charAt(0) == 'P') 
+      { 
+        start_pos = 1; 
+      }
+  
+    if (str.length > 1 && str.charAt(str.length - 1) == 'P') 
+    {
+      end_pos--;
+    }
+  
+    return str.substring(start_pos, end_pos);
+  }
+  
+  console.log(nop("PythonP"));
+  console.log(nop("Python"));
+  console.log(nop("JavaScript"));
+  
 
 
 // Problem #68:
