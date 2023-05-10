67d5421cc2448a7bca1cbac290feba2a68051eec
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-05-10 14:16:08 +0600

    JavaScript program to create a new string using the first and last n characters from a given string. The string length must be greater or equal to n.

--- JS.js
+++ JS.js
@@ -1129,7 +1129,18 @@ function nop(str) {
 
 
 // Problem #68:
-// Write a JavaScript program to convert letters of a given string alphabetically.
+// Write a JavaScript program to create a new string using the first and last n characters from a given string. The string length must be larger than or equal to n.
+
+function two_string(str, n)
+  {
+    first_part = str.substring(0, n);
+    last_part = str.substring(str.length - n);
+    return first_part + last_part;
+ }
+
+console.log(two_string("JavaScript", 2));
+console.log(two_string("JavaScript", 3));
+
 
 
 // Problem #69:
