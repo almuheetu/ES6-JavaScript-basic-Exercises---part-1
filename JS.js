2c3442c4e9cbeabaa17bcdedb8f037ba9c11d5b9
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-05-11 14:57:02 +0600

    JavaScript program to rotate the elements left of a given array of integers of length 3

--- JS.js
+++ JS.js
@@ -1158,7 +1158,16 @@ console.log(sum_three([0, 8, -11]));
 
 
 // Problem #70:
-// Write a JavaScript program to convert letters of a given string alphabetically.
+// Write a JavaScript program to rotate the elements left in a given array of integers of length 3. 
+
+function rotate_elements_left(array)
+{
+    return [array[1], array[2], array[0]];
+}
+console.log(rotate_elements_left([3, 4, 5]));  
+console.log(rotate_elements_left([0, -1, 2]));  
+console.log(rotate_elements_left([7, 6, 5])); 
+
 
 
 // Problem #71:
