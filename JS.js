bc87e2a77bc260e5ef5fde52660c6e23b31c5d37
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-05-11 14:53:42 +0600

    JavaScript program to compute the sum of three elements of an given integers of length 3.

--- JS.js
+++ JS.js
@@ -1144,7 +1144,17 @@ console.log(two_string("JavaScript", 3));
 
 
 // Problem #69:
-// Write a JavaScript program to convert letters of a given string alphabetically.
+// Write a JavaScript program to compute the sum of three elements of a given array of integers of length 3.
+
+function sum_three(nums)
+{
+  return nums[0] + nums[1] + nums[2];
+}
+
+console.log(sum_three([10, 32, 20]));  
+console.log(sum_three([5, 7, 9])); 
+console.log(sum_three([0, 8, -11]));
+
 
 
 // Problem #70:
